0f3e3c42e358009b58cad97d77785fe332d6cadc
yigitkarakurt <yigitkarakurt35@gmail.com>
2022-11-24 19:35:13 +0300

    get datas from firestore

--- InstaCloneFirebase/FeedViewController.swift
+++ InstaCloneFirebase/FeedViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -13,25 +14,82 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     @IBOutlet weak var tableView: UITableView!
     
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
         
+        getDataFromFirestore()
+        
     }
+    func makeAlert(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
+    
+    func getDataFromFirestore(){
+        let fireStoreDatabase = Firestore.firestore()
+        
+        fireStoreDatabase.collection("Posts").addSnapshotListener { snapshot, error in
+            
+            if error != nil{
+                
+                print(error?.localizedDescription as Any)
+                //self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
+                
+            }else{
+                
+                if snapshot?.isEmpty != true && snapshot != nil {
+                   
+                    for document in snapshot!.documents{
+                        let documentID = document.documentID
+                    
+                        if let postedBy = document.get("postedBy") as? String{
+                            self.userEmailArray.append(postedBy)
+                            
+                        }
+                        if let postComment = document.get("postComment") as? String{
+                            self.userCommentArray.append(postComment)
+                            
+                        }
+                        if let likes = document.get("likes") as? Int{
+                            self.likeArray.append(likes)
+                            
+                        }
+                        if let imageUrl = document.get("imageUrl") as? String{
+                            self.userImageArray.append(imageUrl)
+                            
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+            
+        }
+        
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.commentLabel.text = "love you!"
-        cell.likeLabel.text = "4"
-        cell.userEmailLabel.text = "yigitkarakurt@gmail.com"
+        cell.commentLabel.text = userCommentArray[indexPath.row]
+        cell.likeLabel.text = String(likeArray[indexPath.row])
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
         cell.userImageView.image = UIImage(named: "4797305-200.jpg")
+        
         return cell
     }
     
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userEmailArray.count
 
     }
 
